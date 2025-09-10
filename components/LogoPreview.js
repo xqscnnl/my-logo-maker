947adac46b69e9c944dd41440dbb88d42e7d4435
XQSQF <johny.r.f@gmail.com>
2025-09-10 02:10:37 +0200

    index

--- components/LogoPreview.js
+++ components/LogoPreview.js
@@ -57,7 +57,7 @@ const LogoPreview = ({ designConfig }) => {
     const size = 40; // 单个基元的大小
     const center = size / 2;
 
-    console.log(shape);
+    console.log(index);
     
     switch(shape[index]) {
       case 'circle':
